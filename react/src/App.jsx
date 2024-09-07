84201fb281521858d6a404e5d802064e05c3559e
Awais Shahbaz <awaisshahbazrock@gmail.com>
2024-09-06 17:38:40 -0700

    reactPortals

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,8 +1,12 @@
 import UseRef from "./components/UseRef";
+import Portals from "./components/portals";
 function App() {
   return (
     <>
       <UseRef />
+      <div id="modal">
+        <Portals />
+      </div>
     </>
   );
 }
